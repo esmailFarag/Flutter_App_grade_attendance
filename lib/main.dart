2fb1b208ad0aba1a781a60517b70a2335446df12
esmailFarag <33552656+esmailFarag@users.noreply.github.com>
2019-04-23 03:16:32 +0200

    Testing App

---
+++ lib/main.dart
@@ -0,0 +1,234 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'dart:convert';
+
+class Spacecraft {
+  final String id, fullname, finalgrade;
+  final String firstname, lastname, email;
+
+  Spacecraft({
+    this.id,
+    this.firstname,
+    this.lastname,
+    this.email,
+    this.fullname,
+    this.finalgrade,
+  });
+
+  factory Spacecraft.fromJson(Map<String, dynamic> jsonData) {
+    return Spacecraft(
+      id: jsonData['id'],
+      firstname: jsonData['firstname'],
+      lastname: jsonData['lastname'],
+      email: jsonData['email'],
+      fullname: jsonData['fullname'],
+      finalgrade: jsonData['finalgrade'],
+
+      // imageUrl: "http://192.168.1.5/PHP/spacecrafts/images/"+jsonData['image_url'],
+    );
+  }
+}
+
+class CustomListView extends StatelessWidget {
+  final List<Spacecraft> spacecrafts;
+
+  CustomListView(this.spacecrafts);
+
+  Widget build(context) {
+    return ListView.builder(
+      itemCount: spacecrafts.length,
+      itemBuilder: (context, int currentIndex) {
+        return createViewItem(spacecrafts[currentIndex], context);
+      },
+    );
+  }
+
+  Widget createViewItem(Spacecraft spacecraft, BuildContext context) {
+    return new ListTile(
+        title: new Card(
+          elevation: 5.0,
+          child: new Container(
+            decoration: BoxDecoration(border: Border.all(color: Colors.orange)),
+            padding: EdgeInsets.all(20.0),
+            margin: EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  //  child: Image.network(spacecraft.imageUrl),
+                  padding: EdgeInsets.only(bottom: 8.0),
+                ),
+                Row(children: <Widget>[
+                  Padding(
+                      child: Text(
+                        spacecraft.id,
+                        style: new TextStyle(fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.right,
+                      ),
+                      padding: EdgeInsets.all(1.0)),
+                  Text(" | "),
+                  Padding(
+                      child: Text(
+                        spacecraft.firstname,
+                        style: new TextStyle(fontStyle: FontStyle.italic),
+                        textAlign: TextAlign.right,
+                      ),
+                      padding: EdgeInsets.all(1.0)),
+                ]),
+              ],
+            ),
+          ),
+        ),
+        onTap: () {
+          //We start by creating a Page Route.
+          //A MaterialPageRoute is a modal route that replaces the entire
+          //screen with a platform-adaptive transition.
+          var route = new MaterialPageRoute(
+            builder: (BuildContext context) =>
+                new SecondScreen(value: spacecraft),
+          );
+          //A Navigator is a widget that manages a set of child widgets with
+          //stack discipline.It allows us navigate pages.
+          Navigator.of(context).push(route);
+        });
+  }
+}
+
+//Future is n object representing a delayed computation.
+Future<List<Spacecraft>> downloadJSON() async {
+  final jsonEndpoint = "http://192.168.1.5/PHP/spacecrafts";
+
+  final response = await get(jsonEndpoint);
+
+  if (response.statusCode == 200) {
+    List spacecrafts = json.decode(response.body);
+    return spacecrafts
+        .map((spacecraft) => new Spacecraft.fromJson(spacecraft))
+        .toList();
+  } else
+    throw Exception('We were not able to successfully download the json data.');
+}
+
+class SecondScreen extends StatefulWidget {
+  final Spacecraft value;
+
+  SecondScreen({Key key, this.value}) : super(key: key);
+
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('Student Info')),
+      body: new Container(
+        child: new Center(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                child: new Text(
+                  'Grade Details',
+                  style: new TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 20.0),
+                  textAlign: TextAlign.center,
+                ),
+                padding: EdgeInsets.only(bottom: 14.0),
+              ),
+              Padding(
+                child: new Text(
+                  'id : ${widget.value.id}',
+                  style: new TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.left,
+                ),
+                padding: EdgeInsets.all(14.0),
+              ),
+              Padding(
+                child: new Text(
+                  'firstname : ${widget.value.firstname}',
+                  style: new TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.left,
+                ),
+                padding: EdgeInsets.all(14.0),
+              ),
+              Padding(
+                child: new Text(
+                  'lastname : ${widget.value.lastname}',
+                  style: new TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.left,
+                ),
+                padding: EdgeInsets.all(14.0),
+              ),
+              Padding(
+                child: new Text(
+                  'email : ${widget.value.email}',
+                  style: new TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.left,
+                ),
+                padding: EdgeInsets.all(14.0),
+              ),
+              Padding(
+                child: new Text(
+                  'course name : ${widget.value.fullname}',
+                  style: new TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.left,
+                ),
+                padding: EdgeInsets.all(14.0),
+              ),
+              Padding(
+                child: new Text(
+                  'grade : ${widget.value.finalgrade}',
+                  style: new TextStyle(fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.left,
+                ),
+                padding: EdgeInsets.all(14.0),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: new Scaffold(
+        appBar: new AppBar(title: const Text('Course Member')),
+        body: new Center(
+          //FutureBuilder is a widget that builds itself based on the latest snapshot
+          // of interaction with a Future.
+          child: new FutureBuilder<List<Spacecraft>>(
+            future: downloadJSON(),
+            //we pass a BuildContext and an AsyncSnapshot object which is an
+            //Immutable representation of the most recent interaction with
+            //an asynchronous computation.
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List<Spacecraft> spacecrafts = snapshot.data;
+                return new CustomListView(spacecrafts);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              //return  a circular progress indicator.
+              return new CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+//end
